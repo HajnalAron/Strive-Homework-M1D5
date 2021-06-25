633a13025c872ab5a403b27a407d7e1353dd1ee9
HajnalAron <79903244+HajnalAron@users.noreply.github.com>
2021-06-25 14:36:29 +0200

    Exercise 4 done

--- D5.js
+++ D5.js
@@ -45,6 +45,15 @@ const crazyDiff = function(n){
 Write a function "boundary" which accept an integer n and returns true if n is within 20 and 100 (included) or if it's equal to 400.
 */
 
+const boundary = function(n){    
+    if (100 >= n && n >= 20){
+        return true
+    }
+    else if (n === 400){
+        return true
+    }
+}
+
 /* WRITE YOUR CODE HERE */
 
 /* EXERCISE 5
